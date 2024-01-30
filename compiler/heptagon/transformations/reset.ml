390464c84319b754c6a6dbf695bf234ae842e0ac
Adrien Guatto <adrien@guatto.org>
2024-01-30 23:03:51 +0100

    Remove stray tabs from hept/transf/reset

--- compiler/heptagon/transformations/reset.ml
+++ compiler/heptagon/transformations/reset.ml
@@ -81,11 +81,11 @@ let edesc funs ((res,_) as acc) ed =
     | Epre (Some c, e) ->
        let e,_ = Hept_mapfold.exp_it funs acc e in
        (match res with
-	| None -> Epre(Some c, e)
-	| Some _ ->
-	   ifres res
-		 (mk_exp (Econst c) (e.e_ty) ~linearity:Linearity.Ltop)
-		 { e with e_desc = Epre(Some c,e) }), acc
+        | None -> Epre(Some c, e)
+        | Some _ ->
+           ifres res
+                 (mk_exp (Econst c) (e.e_ty) ~linearity:Linearity.Ltop)
+                 { e with e_desc = Epre(Some c,e) }), acc
     | Efby (e1, e2) ->
         let e1,_ = Hept_mapfold.exp_it funs acc e1 in
         let e2,_ = Hept_mapfold.exp_it funs acc e2 in
